981765154677052935b78673052082e4ae3ac2e2
Ijjaz Khan <ijazkhan19932@gmail.com>
2022-12-04 19:44:08 +0500

    mathemtical opertors
    mathemtical opertors

---
+++ FlowControlPython.py
@@ -0,0 +1,16 @@
+# variables
+
+day1 =1
+print(day1)
+
+#numbers : int  float complex
+
+squqre_area =36
+circle_area =6.28
+iota =(-1) ** (1/2)
+
+print(circle_area,circle_area,iota)
+
+# mathemtical opertors
+
+print("Add " ,squqre_area +circle_area )
