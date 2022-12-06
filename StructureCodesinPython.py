681301659f32be2d0ea51f50f4ba429efc6c165f
Ijjaz Khan <ijazkhan19932@gmail.com>
2022-12-06 21:13:10 +0500

    if else
    if else

---
+++ StructureCodesinPython.py
@@ -0,0 +1,14 @@
+print("How to Structure Codes in Python")
+
+def printdata():
+    print(" Python as part of their coding ammunition")
+
+printdata()
+
+email = input("Enter your email : ")
+password  = input("Enter your pass :")
+
+if email =="ijazkhan@gmail.com" and password =="1234":
+    print("Welcome access granted ")
+else :
+    print("sorry we cantvgive you acces ")
